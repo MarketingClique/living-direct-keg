b63c0700e023b4197d0fe9c188d0c2ed79ac9ebc
Pat Ramsey <pat@slash25>
2014-08-28 09:41:16 -0500

    Mega nav code added & tested locally

--- wp-content/themes/kegerator/lib/ca-custom-fields.php
+++ wp-content/themes/kegerator/lib/ca-custom-fields.php
@@ -38,6 +38,121 @@ if(function_exists("register_field_group")){
 	));
 
 	register_field_group(array (
+		'id' => 'acf_branding',
+		'title' => 'Branding',
+		'fields' => array (
+			array (
+				'key' => 'field_532764ced2ade',
+				'label' => 'Images',
+				'name' => '',
+				'type' => 'tab',
+			),
+			array (
+				'key' => 'field_53274271edb4d',
+				'label' => 'Main Logo',
+				'name' => 'main_logo',
+				'type' => 'image',
+				'save_format' => 'url',
+				'preview_size' => 'thumbnail',
+				'library' => 'all',
+			),
+			array (
+				'key' => 'field_53274284edb4e',
+				'label' => 'Mobile Logo',
+				'name' => 'mobile_logo',
+				'type' => 'image',
+				'save_format' => 'url',
+				'preview_size' => 'thumbnail',
+				'library' => 'all',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'options_page',
+					'operator' => '==',
+					'value' => 'acf-options',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'normal',
+			'layout' => 'default',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
+
+	register_field_group(array (
+		'id' => 'acf_header-information',
+		'title' => 'Header Information',
+		'fields' => array (
+			array (
+				'key' => 'field_5318cc6076551',
+				'label' => 'Header Special Image',
+				'name' => 'header_special_image',
+				'type' => 'image',
+				'instructions' => 'This is the image that populates the center area of the header',
+				'save_format' => 'url',
+				'preview_size' => 'thumbnail',
+				'library' => 'all',
+			),
+			array (
+				'key' => 'field_5318cca776552',
+				'label' => 'Header Banner',
+				'name' => 'header_banner',
+				'type' => 'image',
+				'instructions' => 'This is the image that populates the area below the main menu',
+				'save_format' => 'url',
+				'preview_size' => 'thumbnail',
+				'library' => 'all',
+			),
+			array (
+				'key' => 'field_5318cceb76553',
+				'label' => 'Lightbox',
+				'name' => 'lightbox',
+				'type' => 'wysiwyg',
+				'default_value' => '',
+				'toolbar' => 'full',
+				'media_upload' => 'yes',
+			),
+			array (
+				'key' => 'field_533edec222e2a',
+				'label' => 'search query url',
+				'name' => 'search_query_url',
+				'type' => 'text',
+				'instructions' => 'Please specific the search query url for the header site for this site',
+				'default_value' => 'http://www.kegerator.com/on/demandware.store/Sites-Kegerator-Site/default/Search-Show',
+				'placeholder' => 'http://www.kegerator.com/on/demandware.store/Sites-Kegerator-Site/default/Search-Show',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'options_page',
+					'operator' => '==',
+					'value' => 'acf-options',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'normal',
+			'layout' => 'default',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 1,
+	));
+	register_field_group(array (
 		'id' => 'acf_mega-nav-columns',
 		'title' => 'Mega Nav Columns',
 		'fields' => array (
@@ -162,314 +277,9 @@ if(function_exists("register_field_group")){
 			'hide_on_screen' => array (
 			),
 		),
-		'menu_order' => 30,
-	));
-	register_field_group(array (		
-		'id' => 'acf_branding',
-		'title' => 'Branding',
-		'fields' => array (
-			array (
-				'key' => 'field_532764ced2ade',
-				'label' => 'Images',
-				'name' => '',
-				'type' => 'tab',
-			),
-			array (
-				'key' => 'field_53274271edb4d',
-				'label' => 'Main Logo',
-				'name' => 'main_logo',
-				'type' => 'image',
-				'save_format' => 'url',
-				'preview_size' => 'thumbnail',
-				'library' => 'all',
-			),
-			array (
-				'key' => 'field_53274284edb4e',
-				'label' => 'Mobile Logo',
-				'name' => 'mobile_logo',
-				'type' => 'image',
-				'save_format' => 'url',
-				'preview_size' => 'thumbnail',
-				'library' => 'all',
-			),
-		),
-		'location' => array (
-			array (
-				array (
-					'param' => 'options_page',
-					'operator' => '==',
-					'value' => 'acf-options',
-					'order_no' => 0,
-					'group_no' => 0,
-				),
-			),
-		),
-		'options' => array (
-			'position' => 'normal',
-			'layout' => 'default',
-			'hide_on_screen' => array (
-			),
-		),
-		'menu_order' => 0,
+		'menu_order' => 3,
 	));
 
-	register_field_group(array (
-		'id' => 'acf_header-information',
-		'title' => 'Header Information',
-		'fields' => array (
-			array (
-				'key' => 'field_5318cc6076551',
-				'label' => 'Header Special Image',
-				'name' => 'header_special_image',
-				'type' => 'image',
-				'instructions' => 'This is the image that populates the center area of the header',
-				'save_format' => 'url',
-				'preview_size' => 'thumbnail',
-				'library' => 'all',
-			),
-			array (
-				'key' => 'field_5318cca776552',
-				'label' => 'Header Banner',
-				'name' => 'header_banner',
-				'type' => 'image',
-				'instructions' => 'This is the image that populates the area below the main menu',
-				'save_format' => 'url',
-				'preview_size' => 'thumbnail',
-				'library' => 'all',
-			),
-			array (
-				'key' => 'field_5318cceb76553',
-				'label' => 'Lightbox',
-				'name' => 'lightbox',
-				'type' => 'wysiwyg',
-				'default_value' => '',
-				'toolbar' => 'full',
-				'media_upload' => 'yes',
-			),
-			array (
-				'key' => 'field_533edec222e2a',
-				'label' => 'search query url',
-				'name' => 'search_query_url',
-				'type' => 'text',
-				'instructions' => 'Please specific the search query url for the header site for this site',
-				'default_value' => 'http://www.kegerator.com/on/demandware.store/Sites-Kegerator-Site/default/Search-Show',
-				'placeholder' => 'http://www.kegerator.com/on/demandware.store/Sites-Kegerator-Site/default/Search-Show',
-				'prepend' => '',
-				'append' => '',
-				'formatting' => 'html',
-				'maxlength' => '',
-			),
-		),
-		'location' => array (
-			array (
-				array (
-					'param' => 'options_page',
-					'operator' => '==',
-					'value' => 'acf-options',
-					'order_no' => 0,
-					'group_no' => 0,
-				),
-			),
-		),
-		'options' => array (
-			'position' => 'normal',
-			'layout' => 'default',
-			'hide_on_screen' => array (
-			),
-		),
-		'menu_order' => 1,
-	));
-<<<<<<< HEAD
-	
-/*	register_field_group(array (
-=======
-
-	register_field_group(array (
->>>>>>> 4f39753126e07da6f778f9317733759046508be9
-		'id' => 'acf_mega-section',
-		'title' => 'Mega Dropdown',
-		'fields' => array (
-			array (
-				'key' => 'field_53061b4c535bc',
-				'label' => 'Columns',
-				'name' => 'columns',
-				'type' => 'repeater',
-				'sub_fields' => array (
-					array (
-						'key' => 'field_53061b5e535bd',
-						'label' => 'Heading',
-						'name' => 'heading',
-						'type' => 'text',
-						'column_width' => '',
-						'default_value' => 'Product Knowledge',
-						'placeholder' => 'Product Knowledge',
-						'prepend' => '',
-						'append' => '',
-						'formatting' => 'none',
-						'maxlength' => '',
-					),
-					array (
-						'key' => 'field_53061b7f535be',
-						'label' => 'Content',
-						'name' => 'content',
-						'type' => 'textarea',
-						'column_width' => '',
-						'default_value' => 'Ready to do some research? Learn everything there is to know before you buy.',
-						'placeholder' => 'Ready to do some research? Learn everything		there is to know before you buy.',
-						'maxlength' => '',
-						'formatting' => 'br',
-					),
-					array (
-						'key' => 'field_53061b9f535bf',
-						'label' => 'Sub Heading',
-						'name' => 'sub_heading',
-						'type' => 'text',
-						'column_width' => '',
-						'default_value' => 'Start your research here',
-						'placeholder' => 'Start your research here',
-						'prepend' => '',
-						'append' => '',
-						'formatting' => 'none',
-						'maxlength' => '',
-					),
-				),
-				'row_min' => 3,
-				'row_limit' => 3,
-				'layout' => 'row',
-				'button_label' => 'Add Column',
-			),
-			array (
-				'key' => 'field_53061bde535c0',
-				'label' => 'Product Category',
-				'name' => 'product_category',
-				'type' => 'repeater',
-				'sub_fields' => array (
-					array (
-						'key' => 'field_53061bf1535c1',
-						'label' => 'Category Name',
-						'name' => 'category_name',
-						'type' => 'taxonomy',
-						'column_width' => '',
-						'taxonomy' => 'category',
-						'field_type' => 'select',
-						'allow_null' => 0,
-						'load_save_terms' => 0,
-						'return_format' => 'id',
-						'multiple' => 0,
-					),
-					array (
-						'key' => 'field_53061c58535c2',
-						'label' => 'Category Link',
-						'name' => 'category_link',
-						'type' => 'text',
-						'column_width' => '',
-						'default_value' => 'http://www.kegerator.com/',
-						'placeholder' => 'http://www.kegerator.com/',
-						'prepend' => '',
-						'append' => '',
-						'formatting' => 'none',
-						'maxlength' => '',
-					),
-				),
-				'row_min' => 0,
-				'row_limit' => '',
-				'layout' => 'row',
-				'button_label' => 'Add Category',
-			),
-			array (
-				'key' => 'field_530618c0b7015',
-				'label' => 'Product Articles',
-				'name' => 'product_articles',
-				'type' => 'repeater',
-				'instructions' => 'Choose which articles from the Product Knowledge Category you would like to place on the home page',
-				'sub_fields' => array (
-					array (
-						'key' => 'field_530618ffb7017',
-						'label' => 'Article',
-						'name' => 'article',
-						'type' => 'relationship',
-						'column_width' => '',
-						'return_format' => 'object',
-						'post_type' => array (
-							0 => 'post',
-						),
-						'taxonomy' => array (
-							0 => 'category:1',
-						),
-						'filters' => array (
-							0 => 'search',
-						),
-						'result_elements' => array (
-							0 => 'post_title',
-						),
-						'max' => '',
-					),
-				),
-				'row_min' => 0,
-				'row_limit' => '',
-				'layout' => 'row',
-				'button_label' => 'Add Article',
-			),
-			array (
-				'key' => 'field_53061847a82ca',
-				'label' => 'Lifestyle Articles',
-				'name' => 'lifestyle_articles',
-				'type' => 'repeater',
-				'instructions' => 'Choose which articles from the Lifestyle Category you would like to place on the home page',
-				'sub_fields' => array (
-					array (
-						'key' => 'field_53061888a82cb',
-						'label' => 'Article',
-						'name' => 'article',
-						'type' => 'relationship',
-						'column_width' => '',
-						'return_format' => 'object',
-						'post_type' => array (
-							0 => 'post',
-						),
-						'taxonomy' => array (
-							0 => 'category:2',
-						),
-						'filters' => array (
-							0 => 'search',
-						),
-						'result_elements' => array (
-							0 => 'post_title',
-						),
-						'max' => '',
-					),
-				),
-				'row_min' => 0,
-				'row_limit' => '',
-				'layout' => 'row',
-				'button_label' => 'Add Article',
-			),
-		),
-		'location' => array (
-			array (
-				array (
-					'param' => 'options_page',
-					'operator' => '==',
-					'value' => 'acf-options',
-					'order_no' => 0,
-					'group_no' => 0,
-				),
-			),
-		),
-		'options' => array (
-			'position' => 'normal',
-			'layout' => 'default',
-			'hide_on_screen' => array (
-			),
-		),
-		'menu_order' => 2,
-	));
-<<<<<<< HEAD
-*/	
-	
-=======
-
->>>>>>> 4f39753126e07da6f778f9317733759046508be9
 	register_field_group(array (
 		'id' => 'acf_social-widgets',
 		'title' => 'Social Widgets',
