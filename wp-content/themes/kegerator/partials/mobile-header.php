5a1aec038cdd337396b9a84076433e64d6026093
Jean Page <jpage@livingdirect.com>
2014-09-16 12:08:08 -0500

    added an expert and read more updates

--- wp-content/themes/kegerator/partials/mobile-header.php
+++ wp-content/themes/kegerator/partials/mobile-header.php
@@ -5,7 +5,7 @@
 		</a>
 	</section>
 	<section class="mobile-shop-link">
-		<a href="http://kegerator.com/on/demandware.store/Sites-Kegerator-Site/default/Cart-Show">shop</a>
+		<a href="http://kegerator.com/">shop</a>
 	</section>
 	<section class="mobile-cart"><a href="http://kegerator.com/on/demandware.store/Sites-Kegerator-Site/default/Cart-Show"><i class="icon-cart"></i></a></section>
 </header>
